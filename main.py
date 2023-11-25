83ddf7e6f3ddd3f7dec8996426778eef77965418
aliis <aliissa.mrak@mail.ru>
2023-11-25 23:39:49 +0300

    ready

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
 import sys
+from PyQt5 import uic
+import random
+from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import *
 
 
-class WordTrick(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.initUI()
+class WordTrick(QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        uic.loadUi('untitled.ui', self)
+        self.k = False
+        self.pushButton.clicked.connect(self.g)
 
-    def initUI(self):
-        self.setGeometry(400, 400, 400, 400)
-        self.setWindowTitle('bye')
 
-        self.p = QPushButton('aло', self)
+    def g(self):
+        self.k = True
+        self.update()
+
+    def paintEvent(self, event):
+        #super().painEvent(event)
+        if self.k:
+            self.w = QPainter(self)
+            self.w.begin(self)
+            self.w.setBrush(QColor(255, 255, 0))
+            self.ran = random.randint(1, 40)
+            for i in range(13):
+                self.w.drawEllipse(random.randint(0, 400), random.randint(0, 400), self.ran * i - self.ran, self.ran * i - self.ran)
+            self.w.end()
 
 
 if __name__ == '__main__':
